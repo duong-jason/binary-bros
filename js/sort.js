742a6f962054f559c60a0c23efe6f7fac3372bc0
homelessturtles <86428780+homelessturtles@users.noreply.github.com>
2023-03-29 22:22:04 -0700

    add third max page
    created styling for third max page. made some changes to third-max.js to match html tags.

--- js/sort.js
+++ js/sort.js
@@ -1,88 +1,105 @@
 function print(msg, id) {
-    if (document.getElementById(id)) {
-        document.getElementById(id).innerHTML += msg + '<br>'
-    }
+  if (document.getElementById(id)) {
+    document.getElementById(id).innerHTML += msg + "<br>";
+  }
 }
 
 function prettify(arr, start, end, color) {
-    var output = ''
-    for (let i = 0; i < start; i++) {
-        output += arr[i] + ', '
-    }
-    output += `<span style='color: ${color};'> ${arr.slice(start, end).join(', ')}</span>`
-    for (let i = end; i < arr.length; i++) {
-        output += ', ' + arr[i]
-    }
-    return output
+  var output = "";
+  for (let i = 0; i < start; i++) {
+    output += arr[i] + ", ";
+  }
+  output += `<span style='color: ${color};'> ${arr
+    .slice(start, end)
+    .join(", ")}</span>`;
+  for (let i = end; i < arr.length; i++) {
+    output += ", " + arr[i];
+  }
+  return output;
 }
 
 function bubble_sort(arr, n) {
-    // FIXME: Expected size=, Got size=1
-    // Happens when correct input to 'element' field but 'size' field wasn't specified
-    if (n === "") {
-        n = null
-    }
-    if (n != arr.length) {
-        message = `Expected size=${n ?? "N.A."}, Got size=${arr.length}`
-        alert(message)
-        throw new RangeError(message)
-    }
+  // FIXME: Expected size=, Got size=1
+  // Happens when correct input to 'element' field but 'size' field wasn't specified
+  if (n === "") {
+    n = null;
+  }
+  if (n != arr.length) {
+    message = `Expected size=${n ?? "N.A."}, Got size=${arr.length}`;
+    alert(message);
+    throw new RangeError(message);
+  }
 
-    for (let i = 0; i < n-1; i++) {
-        for (let j = 0; j < n-i-1; j++) {
-            print(`${i+1}) ` + prettify(arr, j, j+2, (arr[j] > arr[j+1]) ? 'Tomato' : 'MediumSeaGreen'), 'output_1')
-            if (arr[j] > arr[j+1]) {
-                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
-            }
-        }
-        print(`Result: ${arr.join(', ')}`, 'output_1')
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      print(
+        `${i + 1}) ` +
+          prettify(
+            arr,
+            j,
+            j + 2,
+            arr[j] > arr[j + 1] ? "Tomato" : "MediumSeaGreen"
+          ),
+        "output_1"
+      );
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
     }
-    print(`Sorted Array: ${arr.join(', ')}`, 'output_1')
-    return arr
+    print(`Result: ${arr.join(", ")}`, "output_1");
+  }
+  print(`Sorted Array: ${arr.join(", ")}`, "output_1");
+  return arr;
 }
 
-function merge_sort(arr, n=null) {
-    if (n === "") {
-        n = null
-    } else if (n === null) {
-        n = arr.length
-    }
-    if (n != arr.length) {
-        message = `Expected size=${n ?? "N.A."}, Got size=${arr.length}`
-        alert(message)
-        throw new RangeError(message)
-    }
+function merge_sort(arr, n = null) {
+  if (n === "") {
+    n = null;
+  } else if (n === null) {
+    n = arr.length;
+  }
+  if (n != arr.length) {
+    message = `Expected size=${n ?? "N.A."}, Got size=${arr.length}`;
+    alert(message);
+    throw new RangeError(message);
+  }
 
-    let result = null
+  let result = null;
 
-    /* either one element from split or input array with less than 1 element */
-    if (n <= 1) {
-        result = arr
-    } else {
-        const mid = Math.floor(n/2)
-        let [left, right] = [arr.slice(0, mid), arr.slice(mid)]
+  /* either one element from split or input array with less than 1 element */
+  if (n <= 1) {
+    result = arr;
+  } else {
+    const mid = Math.floor(n / 2);
+    let [left, right] = [arr.slice(0, mid), arr.slice(mid)];
 
-        print(`Split Array: ${prettify(arr, mid-1, mid+1, 'Orange')}`, 'output_2')
-        result = merge(merge_sort(left), merge_sort(right))
-    }
+    print(
+      `Split Array: ${prettify(arr, mid - 1, mid + 1, "Orange")}`,
+      "output_2"
+    );
+    result = merge(merge_sort(left), merge_sort(right));
+  }
 
-    print(`Merged Array: ${result.join(', ')}`, 'output_2')
-    return result
+  print(`Merged Array: ${result.join(", ")}`, "output_2");
+  return result;
 }
 
 function merge(a, b) {
-    const n = a.length, m = b.length
-    let c = [], p = 0, q = 0
+  const n = a.length,
+    m = b.length;
+  let c = [],
+    p = 0,
+    q = 0;
 
-    while (p < n && q < m) {
-        c.push((a[p] < b[q]) ? a[p++] : b[q++])
-    }
+  while (p < n && q < m) {
+    c.push(a[p] < b[q] ? a[p++] : b[q++]);
+  }
 
-    /* either sorted array `a` or `b` must be empty and the other with at least one element */
-    return [...c, ...a.slice(p, n), ...b.slice(q, m)]
+  /* either sorted array `a` or `b` must be empty and the other with at least one element */
+  return [...c, ...a.slice(p, n), ...b.slice(q, m)];
 }
 
 module.exports = {
-    bubble: bubble_sort, 
-    merge: merge_sort
-}
+  bubble: bubble_sort,
+  merge: merge_sort,
+};
--- js/third-max.js
+++ js/third-max.js
@@ -1,116 +1,134 @@
 function print(msg, id) {
-    if (document.getElementById(id)) {
-        document.getElementById(id).innerHTML += msg + '<br>'
-    }
+  if (document.getElementById(id)) {
+    document.getElementById(id).innerHTML += msg + "<br>";
+  }
 }
 
 function prettify(arr, start, end, color) {
-    var output = ''
-    for (let i = 0; i < start; i++) {
-        output += arr[i] + ', '
-    }
-    output += `<span style='color: ${color};'> ${arr.slice(start, end).join(', ')}</span>`
-    for (let i = end; i < arr.length; i++) {
-        output += ', ' + arr[i]
-    }
-    return output
+  var output = "";
+  for (let i = 0; i < start; i++) {
+    output += arr[i] + ", ";
+  }
+  output += `<span style='color: ${color};'> ${arr
+    .slice(start, end)
+    .join(", ")}</span>`;
+  for (let i = end; i < arr.length; i++) {
+    output += ", " + arr[i];
+  }
+  return output;
 }
 
-function merge_sort(arr, n=null) {
-    if (n === "") {
-        n = null
-    } else if (n === null) {
-        n = arr.length
-    }
-    if (n != arr.length) {
-        message = `Expected size=${n ?? "N.A."}, Got size=${arr.length}`
-        alert(message)
-        throw new RangeError(message)
-    }
+function merge_sort(arr, n = null) {
+  if (n === "") {
+    n = null;
+  } else if (n === null) {
+    n = arr.length;
+  }
+  if (n != arr.length) {
+    message = `Expected size=${n ?? "N.A."}, Got size=${arr.length}`;
+    alert(message);
+    throw new RangeError(message);
+  }
 
-    let result = null
+  let result = null;
 
-    /* either one element from split or input array with less than 1 element */
-    if (n <= 1) {
-        result = arr
-    } else {
-        const mid = Math.floor(n/2)
-        let [left, right] = [arr.slice(0, mid), arr.slice(mid)]
+  /* either one element from split or input array with less than 1 element */
+  if (n <= 1) {
+    result = arr;
+  } else {
+    const mid = Math.floor(n / 2);
+    let [left, right] = [arr.slice(0, mid), arr.slice(mid)];
 
-        print(`Split Array: ${prettify(arr, mid-1, mid+1, 'Orange')}`, 'output')
-        result = merge(merge_sort(left), merge_sort(right))
-    }
+    print(
+      `Split Array: ${prettify(arr, mid - 1, mid + 1, "Orange")}`,
+      "output_1"
+    );
+    result = merge(merge_sort(left), merge_sort(right));
+  }
 
-    print(`Merged Array: ${result.join(', ')}`, 'output')
-    return result
+  print(`Merged Array: ${result.join(", ")}`, "output_1");
+  return result;
 }
 
 function merge(a, b) {
-    const n = a.length, m = b.length
-    let c = [], p = 0, q = 0
+  const n = a.length,
+    m = b.length;
+  let c = [],
+    p = 0,
+    q = 0;
 
-    while (p < n && q < m) {
-        c.push((a[p] < b[q]) ? a[p++] : b[q++])
-    }
+  while (p < n && q < m) {
+    c.push(a[p] < b[q] ? a[p++] : b[q++]);
+  }
 
-    /* either sorted array `a` or `b` must be empty and the other with at least one element */
-    return [...c, ...a.slice(p, n), ...b.slice(q, m)]
-}
-
-// O(n)
-function optimal_third_max(arr, n) {
-    if (n === "") {
-        n = null
-    }
-    if (n != arr.length) {
-        message = `Expected size=${n ?? "N.A."}, Got size=${arr.length}`
-        alert(message)
-        throw new RangeError(message)
-    } else if (n < 3) {
-        print('Third Max Element: None', 'output')
-        return null
-    }
-
-    let f_max = Number.NEGATIVE_INFINITY
-    let s_max = Number.NEGATIVE_INFINITY
-    let t_max = Number.NEGATIVE_INFINITY
-
-    for (let i = 0; i < n; i++) {
-        if (arr[i] >= f_max) {
-            t_max = s_max, s_max = f_max, f_max = arr[i]
-        } else if (arr[i] >= s_max) {
-            t_max = s_max, s_max = arr[i]
-        } else if (arr[i] >= t_max) {
-            t_max = arr[i]
-        }
-        print(`${i+1}) First Max: ${f_max}, Second Max: ${s_max}, Third Max: ${t_max}`, 'output')
-    }
-    print(`Third Max Element: ${t_max}`, 'output')
-    return t_max
+  /* either sorted array `a` or `b` must be empty and the other with at least one element */
+  return [...c, ...a.slice(p, n), ...b.slice(q, m)];
 }
 
 // O(nlogn)
+function optimal_third_max(arr, n) {
+  if (n === "") {
+    n = null;
+  }
+  if (n != arr.length) {
+    message = `Expected size=${n ?? "N.A."}, Got size=${arr.length}`;
+    alert(message);
+    throw new RangeError(message);
+  } else if (n < 3) {
+    print("Third Max Element: None", "output_2");
+    console.log("Third Max Element: None");
+    return null;
+  }
+
+  let f_max = Number.NEGATIVE_INFINITY;
+  let s_max = Number.NEGATIVE_INFINITY;
+  let t_max = Number.NEGATIVE_INFINITY;
+
+  for (let i = 0; i < n; i++) {
+    if (arr[i] >= f_max) {
+      (t_max = s_max), (s_max = f_max), (f_max = arr[i]);
+    } else if (arr[i] >= s_max) {
+      (t_max = s_max), (s_max = arr[i]);
+    } else if (arr[i] >= t_max) {
+      t_max = arr[i];
+    }
+    print(
+      `${
+        i + 1
+      }) First Max: ${f_max}, Second Max: ${s_max}, Third Max: ${t_max}`,
+      "output_2"
+    );
+    console.log(
+      `${i + 1}) First Max: ${f_max}, Second Max: ${s_max}, Third Max: ${t_max}`
+    );
+  }
+  print(`Third Max Element: ${t_max}`, "output_2");
+  console.log(`Third Max Element: ${t_max}`);
+  return t_max;
+}
+
+// O(n)
 function naive_third_max(arr, n) {
-    if (n === "") {
-        n = null
-    }
-    if (n != arr.length) {
-        message = `Expected size=${n ?? "N.A."}, Got size=${arr.length}`
-        alert(message)
-        throw new RangeError(message)
-    } else if (n < 3) {
-        print('Third Max Element: None', 'output')
-        return null
-    }
+  if (n === "") {
+    n = null;
+  }
+  if (n != arr.length) {
+    message = `Expected size=${n ?? "N.A."}, Got size=${arr.length}`;
+    alert(message);
+    throw new RangeError(message);
+  } else if (n < 3) {
+    print("Third Max Element: None", "output_1");
+    return null;
+  }
 
-    const sorted_arr = merge_sort(arr, n)
-    const t_max = sorted_arr[sorted_arr.length-3]
+  const sorted_arr = merge_sort(arr, n);
+  const t_max = sorted_arr[sorted_arr.length - 3];
 
-    print(`Third Max Element: ${t_max}`, 'output')
-    return t_max
+  print(`Third Max Element: ${t_max}`, "output_1");
+  return t_max;
 }
 
 module.exports = {
-    naive_third_max,
-    optimal_third_max
-}
+  naive_third_max,
+  optimal_third_max,
+};
